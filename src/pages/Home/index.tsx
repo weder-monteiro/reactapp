e2a3cbc665211fa8537fdb7a95c3f2a38972a3d2
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-06-02 08:45:06 -0300

    Finalizando

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles.css';
 import { Card } from '../../components/Card';
 
@@ -10,6 +10,7 @@ type Student = {
 export function Home() {
   const [studentName, setStudentName] = useState<string>();
   const [students, setStudents] = useState<Student[]>([]);
+  const [user, setUser] = useState({name: '', avatar: ''});
 
   function handleAddStudent() {
     const newStudent = {
@@ -24,9 +25,23 @@ export function Home() {
     setStudents(prevState => [...prevState, newStudent]);
   };
 
+  useEffect(() => {
+    fetch('https://api.github.com/users/weder-monteiro')
+    .then(response => response.json())
+    .then(data => {
+      setUser({name: data.name, avatar: data.avatar_url});
+    })
+  }, []);
+
   return (
     <div className="container">
-      <h1>Lista de presença</h1>
+      <header>
+        <h1>Lista de presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de perfil" />
+        </div>
+      </header>
       <input type="text" placeholder="Digite o nome..." onChange={e => setStudentName(e.target.value)}/>
       <button type="button" onClick={handleAddStudent}>Adicionar</button>
 
